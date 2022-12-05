dc1ee826f447d5be16f4d90afaa970aba59d9e03
AbdallaEbrahimElanoz <anaabdoa319@gmail.com>
2022-12-05 20:23:13 +0200

    add middlwares for loggin security

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,30 @@
 import express, { Application, Request, Response } from 'express'
-
+import morgan from 'morgan'
+import helmet from 'helmet'
+import rateLimit from 'express-rate-limit'
 const PORT = 3000
 // create an instance server
 const app: Application = express()
+
+//middleware to parse incoming requests 
+app.use(express.json())
+// HTTP request logger middleware
+app.use(morgan('common'))
+
+// HTTP security middleware headers
+app.use(helmet())
+// Basic rate-limiting middleware for Express
+// Apply the rate limiting middleware to all requests
+app.use(
+  rateLimit({
+    windowMs: 60 * 1000, // 1 minute
+    max: 100, // Limit each IP to 2 requests per `window` (here, per 1 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    message: 'Too many accounts created from this IP, please try again after an hour',
+  })
+)
+
 
 // add routing for / path
 app.get('/', (req: Request, res: Response) => {
